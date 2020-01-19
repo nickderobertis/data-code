0164c7dccf0717e2513de4f611691dbb52968953
Nick DeRobertis <whoopnip@gmail.com>
2020-01-19 12:02:28 -0500

    Move dero.ext_pandas to pd_utils

--- dero/data/ff/create/minus.py
+++ dero/data/ff/create/minus.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from typing import List
 
-from dero.ext_pandas.pdutils import _to_list_if_str
+from pd_utils.pdutils import _to_list_if_str
 from dero.data.ff.fftypes import (
     DictofStrsandStrLists,
     TwoStrTuple,
