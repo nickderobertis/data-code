360f9a68ccd63de9dfe6778e16bc09d512a8edfc
Nick DeRobertis <whoopnip@gmail.com>
2020-02-09 12:41:48 -0500

    Implement data transform funcs through loader in source, and add tests

--- datacode/models/loader.py
+++ datacode/models/loader.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
 import pandas as pd
@@ -51,11 +52,15 @@ class DataLoader:
     def apply_transforms(self, df: pd.DataFrame) -> pd.DataFrame:
         if not self.source.columns:
             return df
+
+        # Create temporary source so that transform can have access to df and all columns with one object
+        temp_source = deepcopy(self.source)
+        temp_source.df = df
+        temp_source.name = '_temp_source_for_transform'
         for column in self.source.columns:
-            variable = column.variable
-            for transform in variable.applied_transforms:
-                df[variable.name] = transform.data_func(df[variable.name])
-        return df
+            for transform in column.variable.applied_transforms:
+                temp_source = transform.apply_transform_to_source(temp_source, column)
+        return temp_source.df
 
     def pre_transform_clean_up(self, df: pd.DataFrame) -> pd.DataFrame:
         return df
--- tests/test_source.py
+++ tests/test_source.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Any, Callable
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
@@ -10,6 +10,32 @@ from datacode.models.source import DataSource
 from datacode.models.variables import Variable
 from datacode.models.variables.transform import Transform
 from tests.utils import GENERATED_PATH
+
+
+def transform_cell_data_func(col: Column, cell: Any) -> Any:
+    if isinstance(cell, str):
+        return cell
+
+    return cell + 1
+
+
+def transform_series_data_func(col: Column, series: pd.Series) -> pd.Series:
+    return series + 1
+
+
+def transform_dataframe_data_func(col: Column, df: pd.DataFrame) -> pd.DataFrame:
+    df[col.variable.name] = df[col.variable.name] + 1
+    return df
+
+
+def transform_source_data_func(col: Column, source: DataSource) -> DataSource:
+    # Extra unnecessary logic to access source.columns to test looking up columns
+    cols = source.columns
+    for this_col in cols:
+        if not this_col.variable.key == col.variable.key:
+            continue
+        source.df[this_col.variable.name] = source.df[this_col.variable.name] + 1
+    return source
 
 
 class SourceTest:
@@ -38,8 +64,10 @@ class SourceTest:
         columns=['A_1', 'B_1', 'C']
     )
     transform_name_func = lambda x: f'{x}_1'
-    transform_data_func = lambda x: x + 1
-    transform = Transform('add_one', transform_name_func, transform_data_func)
+    transform_cell = Transform('add_one_cell', transform_name_func, transform_cell_data_func, data_func_target='cell')
+    transform_series = Transform('add_one_series', transform_name_func, transform_series_data_func, data_func_target='series')
+    transform_dataframe = Transform('add_one_df', transform_name_func, transform_dataframe_data_func, data_func_target='dataframe')
+    transform_source = Transform('add_one_source', transform_name_func, transform_source_data_func, data_func_target='source')
     csv_path = os.path.join(GENERATED_PATH, 'data.csv')
 
     def setup_method(self):
@@ -56,16 +84,30 @@ class SourceTest:
         config_dict.update(kwargs)
         return DataSource(**config_dict)
 
+    def get_transform(self, func_type: str) -> Transform:
+        if func_type == 'cell':
+            return self.transform_cell
+        elif func_type == 'series':
+            return self.transform_series
+        elif func_type == 'dataframe':
+            return self.transform_dataframe
+        elif func_type == 'source':
+            return self.transform_source
+        else:
+            raise ValueError(
+                f'could not look up func_type {func_type}, should be one of cell, series, dataframe, source')
+
     def create_csv(self, df: Optional[pd.DataFrame] = None):
         if df is None:
             df = self.test_df
         df.to_csv(self.csv_path, index=False)
 
-    def create_variables(self, transform_data: bool = False) -> Tuple[Variable, Variable, Variable]:
+    def create_variables(self, transform_data: str = '') -> Tuple[Variable, Variable, Variable]:
         if transform_data:
+            transform = self.get_transform(transform_data)
             transform_dict = dict(
-                available_transforms=[self.transform],
-                applied_transforms=[self.transform],
+                available_transforms=[transform],
+                applied_transforms=[transform],
             )
         else:
             transform_dict = {}
@@ -75,7 +117,7 @@ class SourceTest:
         c = Variable('c', 'C', dtype='categorical')
         return a, b, c
 
-    def create_columns(self, transform_data: bool = False) -> Tuple[Column, Column, Column]:
+    def create_columns(self, transform_data: str = '') -> Tuple[Column, Column, Column]:
         a, b, c = self.create_variables(transform_data=transform_data)
         ac = Column(a)
         bc = Column(b)
@@ -108,8 +150,26 @@ class TestLoadSource(SourceTest):
         ds = self.create_source(df=None, columns=all_cols)
         assert_frame_equal(ds.df, self.expect_loaded_df_rename_only)
 
-    def test_load_with_columns_and_transforms(self):
+    def test_load_with_columns_and_transform_cell(self):
         self.create_csv()
-        all_cols = self.create_columns(transform_data=True)
+        all_cols = self.create_columns(transform_data='cell')
+        ds = self.create_source(df=None, columns=all_cols)
+        assert_frame_equal(ds.df, self.expect_loaded_df_with_transform)
+
+    def test_load_with_columns_and_transform_series(self):
+        self.create_csv()
+        all_cols = self.create_columns(transform_data='series')
+        ds = self.create_source(df=None, columns=all_cols)
+        assert_frame_equal(ds.df, self.expect_loaded_df_with_transform)
+
+    def test_load_with_columns_and_transform_dataframe(self):
+        self.create_csv()
+        all_cols = self.create_columns(transform_data='dataframe')
+        ds = self.create_source(df=None, columns=all_cols)
+        assert_frame_equal(ds.df, self.expect_loaded_df_with_transform)
+
+    def test_load_with_columns_and_transform_source(self):
+        self.create_csv()
+        all_cols = self.create_columns(transform_data='source')
         ds = self.create_source(df=None, columns=all_cols)
         assert_frame_equal(ds.df, self.expect_loaded_df_with_transform)
