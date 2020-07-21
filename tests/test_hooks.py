e68976ea41ad44f57426aaa9cc4ea5a50443835b
Nick DeRobertis <whoopnip@gmail.com>
2020-07-21 05:52:57 -0400

    Add functionality to chain hooks rather than replacing

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -15,6 +15,10 @@ def increase_counter_hook(*args, **kwargs):
     return kwargs
 
 
+def raise_error(*args, **kwargs):
+    raise ValueError
+
+
 def increase_counter_hook_discard_first_arg(*args):
     increase_counter_hook()
     return args[1:]
@@ -53,12 +57,33 @@ class TestPipelineHooks(HooksTest):
         dgp.execute()
         assert COUNTER == counter_value
 
+    def test_chain_at_end(self):
+        counter_value = COUNTER
+        dc_hooks.on_begin_execute_pipeline = increase_counter_hook
+        dc_hooks.chain('on_begin_execute_pipeline', raise_error)
+        dgp = self.create_generator_pipeline()
+        with self.assertRaises(ValueError) as cm:
+            dgp.execute()
+        assert COUNTER == counter_value + 1
+
+    def test_chain_at_beginning(self):
+        counter_value = COUNTER
+        dc_hooks.on_begin_execute_pipeline = increase_counter_hook
+        dc_hooks.chain('on_begin_execute_pipeline', raise_error, at_end=False)
+        dgp = self.create_generator_pipeline()
+        with self.assertRaises(ValueError) as cm:
+            dgp.execute()
+        assert COUNTER == counter_value
+
     def test_on_begin_execute_pipeline(self):
         counter_value = COUNTER
         dc_hooks.on_begin_execute_pipeline = increase_counter_hook
         dgp = self.create_generator_pipeline()
         dgp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_begin_execute_pipeline', increase_counter_hook)
+        dgp.execute()
+        assert COUNTER == counter_value + 3
 
     def test_on_end_execute_pipeline(self):
         counter_value = COUNTER
@@ -66,6 +91,9 @@ class TestPipelineHooks(HooksTest):
         dgp = self.create_generator_pipeline()
         dgp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_end_execute_pipeline', increase_counter_hook)
+        dgp.execute()
+        assert COUNTER == counter_value + 3
 
     def test_on_begin_execute_operation(self):
         counter_value = COUNTER
@@ -73,6 +101,10 @@ class TestPipelineHooks(HooksTest):
         dgp = self.create_generator_pipeline()
         dgp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_begin_execute_operation', increase_counter_hook)
+        dgp = self.create_generator_pipeline()
+        dgp.execute()
+        assert COUNTER == counter_value + 3
 
     def test_on_end_execute_operation(self):
         counter_value = COUNTER
@@ -80,6 +112,10 @@ class TestPipelineHooks(HooksTest):
         dgp = self.create_generator_pipeline()
         dgp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_end_execute_operation', increase_counter_hook)
+        dgp = self.create_generator_pipeline()
+        dgp.execute()
+        assert COUNTER == counter_value + 3
 
     def test_on_begin_load_source(self):
         counter_value = COUNTER
@@ -88,6 +124,10 @@ class TestPipelineHooks(HooksTest):
         ds = self.create_source(df=None)
         df = ds.df
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_begin_load_source', increase_counter_hook)
+        ds = self.create_source(df=None)
+        df = ds.df
+        assert COUNTER == counter_value + 3
 
     def test_on_end_load_source(self):
         dc_hooks.on_end_load_source = increment_df_a_variable_hook
@@ -95,6 +135,10 @@ class TestPipelineHooks(HooksTest):
         ds = self.create_source(df=None)
         df = ds.df
         assert_frame_equal(df, self.expect_df_no_rename_a_plus_1)
+        dc_hooks.chain('on_end_load_source', increment_df_a_variable_hook)
+        ds = self.create_source(df=None)
+        df = ds.df
+        assert_frame_equal(df, self.expect_df_no_rename_a_plus_2)
 
     def test_on_begin_apply_variable_transform(self):
         counter_value = COUNTER
@@ -104,6 +148,11 @@ class TestPipelineHooks(HooksTest):
         ds = self.create_source(df=None, columns=cols)
         df = ds.df
         assert COUNTER == counter_value + 2
+        dc_hooks.chain('on_begin_apply_variable_transform', increase_counter_hook_discard_first_arg)
+        cols = self.create_columns(transform_data='series')
+        ds = self.create_source(df=None, columns=cols)
+        df = ds.df
+        assert COUNTER == counter_value + 6
 
     def test_on_end_apply_variable_transform(self):
         counter_value = COUNTER
@@ -113,6 +162,11 @@ class TestPipelineHooks(HooksTest):
         ds = self.create_source(df=None, columns=cols)
         df = ds.df
         assert COUNTER == counter_value + 2
+        dc_hooks.chain('on_end_apply_variable_transform', increase_counter_hook_return_only_second_arg)
+        cols = self.create_columns(transform_data='series')
+        ds = self.create_source(df=None, columns=cols)
+        df = ds.df
+        assert COUNTER == counter_value + 6
 
     def test_on_begin_apply_source_transform(self):
         counter_value = COUNTER
@@ -120,6 +174,10 @@ class TestPipelineHooks(HooksTest):
         dtp = self.create_transformation_pipeline()
         dtp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_begin_apply_source_transform', increase_counter_hook_return_only_second_arg)
+        dtp = self.create_transformation_pipeline()
+        dtp.execute()
+        assert COUNTER == counter_value + 3
 
     def test_on_end_apply_source_transform(self):
         counter_value = COUNTER
@@ -127,3 +185,7 @@ class TestPipelineHooks(HooksTest):
         dtp = self.create_transformation_pipeline()
         dtp.execute()
         assert COUNTER == counter_value + 1
+        dc_hooks.chain('on_end_apply_source_transform', increase_counter_hook_return_only_second_arg)
+        dtp = self.create_transformation_pipeline()
+        dtp.execute()
+        assert COUNTER == counter_value + 3
